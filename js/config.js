dce5c37aaa4b23e75c1b9ecc34fbd2915d809dd5
nikil56 <nikhilnibu72004@gmail.com>
2025-04-15 10:49:46 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_Yf4knbkvE', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4p6j0m1kat9t6bh6ve4c4colbp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
